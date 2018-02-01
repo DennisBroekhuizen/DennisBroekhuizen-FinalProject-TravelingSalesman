43945f5bd7cda74fe7d9023cea0904b2b8294625
Dennis Broekhuizen <dennisbroekhuizen@hotmail.com>
2018-02-01 17:29:29 +0100

    Delete unused files

--- Project/TravelingSalesman/Models/SharedFunctions.swift
+++
@@ -1,9 +0,0 @@
-//
-//  SharedFunctions.swift
-//  TravelingSalesman
-//
-//  Created by Dennis Broekhuizen on 30-01-18.
-//  Copyright © 2018 Dennis Broekhuizen. All rights reserved.
-//
-
-import Foundation
